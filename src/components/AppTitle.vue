0950bccceea31bf4d3e8e9c69f220a6f62816071
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-16 01:18:18 +0200

    added AppTitle.vue

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <h1>La mia prima app con Vite!</h1>
+    </header>
+</template>
+
+<style scoped>
+    h1{
+        text-align: center;
+        color: red;
+    }
+</style>
