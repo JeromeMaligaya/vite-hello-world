3924976b2c77846df02e0af930390137b1b4ade5
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-16 01:28:54 +0200

    fixed from script setup to script on App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
-<script setup>
-// import HelloWorld from './components/HelloWorld.vue'
+<script>
 import AppTitle from './components/AppTitle.vue';
+
+export default {
+  components:{
+    AppTitle
+  },
+  data(){
+    return{
+
+    }
+  }
+}
 </script>
 
 <template>
   <AppTitle />
-  <!-- <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
 <style scoped>
